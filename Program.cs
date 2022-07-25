57cbbd852e50480a76ffbd0051a74b1b3fa4bbc1
Caio Brdaric <caiobrdaric@gmail.com>
2022-07-25 00:05:41 -0300

    Retirando quebras de linha

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-double CalculoINSS(double salario)
+﻿double CalculoINSS(double salario)
 {
     double c_salario = salario; 
     double desconto = 0;
@@ -8,22 +7,18 @@ double CalculoINSS(double salario)
     {
         desconto = c_salario - (c_salario * 0.925);
     }
-
     if (salario > 1212.01  && salario <= 2427.35)
     {
         desconto = c_salario - (c_salario * 0.91);
     }
-
     if (salario >= 2427.36 && salario <= 3641.03)
     {
         desconto = c_salario - (c_salario * 0.88);
     }
-
     if (salario >= 3641.04 && salario <= 7087.22)
     {
         desconto = c_salario - (c_salario *  0.86);
     }
-
     return desconto;
 }
 double CalculoIR(double salario)
@@ -34,31 +29,26 @@ double CalculoIR(double salario)
     if (c_salario <= 1903.98)
     {
     }
-
     if (c_salario >= 1903.99 && c_salario <= 2826.65)
     {
         desconto = ((c_salario - (c_salario * 0.925)) - 142.80);
 
     }
-
     if (c_salario >= 2826.66 && c_salario <= 3751.05)
     {
         desconto = (c_salario - (c_salario * 0.85)) - 354.80;
 
     }
-
     if (c_salario >= 3751.06 && c_salario <= 4664.68)
     {
         desconto = (c_salario - (c_salario * 0.775)) - 636.13;
 
     }
-
     if (c_salario >= 4664.69)
     {
         desconto = (c_salario - (c_salario * 0.725)) - 869.36;
 
     }
-
     return desconto;
 }
 
@@ -67,8 +57,8 @@ double salario = Convert.ToDouble(Console.ReadLine());
 
 var descontoINSS = CalculoINSS(salario);
 var descontoIR = CalculoIR(salario);
-
 double salarioliquido = salario - descontoIR - descontoINSS;
+
 Console.WriteLine($"Desconto do IR: {descontoIR}");
 Console.WriteLine($"Desconto do INSS: {descontoINSS}");
 Console.WriteLine($"Seu salario é {salarioliquido}");
